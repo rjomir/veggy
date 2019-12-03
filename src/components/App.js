aafe72acb4a20c1a15aabbb476ba5f2b7859767c
Rossnerr <metei1997vasile@gmail.com>
2019-12-03 11:46:57 +0200

    Empty the vmetei-master

--- src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import Layout from './layout/layout'
-
-const App = () => <Layout />
-
-export default App
--- src/redux/sagas/sagas.js
+++
@@ -1,9 +0,0 @@
-import { all, call } from 'redux-saga/effects'
-import productsSagas from './products-sagas'
-import confirmDialogSaga from './confirm-dialog-saga'
-
-function* rootSaga() {
-  yield all([call(productsSagas), call(confirmDialogSaga)])
-}
-
-export default rootSaga
